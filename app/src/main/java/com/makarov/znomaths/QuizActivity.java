91eb3167f40b564878a9ef9fcad516aa13962a84
navimakarov <makarov18042003@gmail.com>
2020-05-01 19:40:52 +0300

    Add progressBar

--- app/src/main/java/com/makarov/znomaths/QuizActivity.java
+++ app/src/main/java/com/makarov/znomaths/QuizActivity.java
@@ -7,20 +7,24 @@ import android.util.Log;
 import java.lang.reflect.Type;
 import java.util.List;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class QuizActivity extends AppCompatActivity {
 
     private String jsonFileString;
+    private ProgressBar pbHorizontal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        pbHorizontal = (ProgressBar) findViewById(R.id.pb);
+        pbHorizontal.setProgress(33);
+
         jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "db.json");
         Gson gson = new Gson();
         Type listQuestionType = new TypeToken<List<Question>>() { }.getType();
         List<Question> questions = gson.fromJson(jsonFileString, listQuestionType);
-
     }
 }
