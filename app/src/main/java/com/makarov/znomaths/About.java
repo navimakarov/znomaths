5735507a7384eb9d9826ed7baf33d6c384addfcc
navimakarov <makarov18042003@gmail.com>
2020-05-02 13:26:10 +0300

    Added the ability to copy e-mail on about page

--- app/src/main/java/com/makarov/znomaths/About.java
+++ app/src/main/java/com/makarov/znomaths/About.java
@@ -2,9 +2,13 @@ package com.makarov.znomaths;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class About extends AppCompatActivity {
 
@@ -17,5 +21,13 @@ public class About extends AppCompatActivity {
     public void menu_button_onCLick(View view){
         Intent intent_menu = new Intent(this, MainActivity.class);
         startActivity(intent_menu);
+    }
+
+    public void contact_button_onClick(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("e-mail", "makarov18042003@gmail.com");
+        clipboard.setPrimaryClip(clip);
+        Toast toast = Toast.makeText(this, "The e-mail was copied", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
