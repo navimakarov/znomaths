7759afdcaf620175122ec1694a6f19b959487b38
navimakarov <makarov18042003@gmail.com>
2020-05-03 13:35:34 +0300

    Admob

--- app/src/main/java/com/makarov/znomaths/QuizActivity.java
+++ app/src/main/java/com/makarov/znomaths/QuizActivity.java
@@ -1,6 +1,14 @@
 package com.makarov.znomaths;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.MobileAdsInitProvider;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
@@ -35,10 +43,35 @@ public class QuizActivity extends AppCompatActivity {
     private Game game;
     private Player player;
 
+    private AdView adView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        adView = findViewById(R.id.adView);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdRequest request = new AdRequest.Builder().build();
+        adView.loadAd(request);
+
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                System.out.println("onAdLoaded() CALLBACK");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+                System.out.println("onAdFailedToLoad() CALLBACK code: " + errorCode);
+            }
+        });
 
         pb = (ProgressBar) findViewById(R.id.pb);
 
